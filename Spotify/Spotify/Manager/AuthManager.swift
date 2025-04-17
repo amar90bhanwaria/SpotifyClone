9790c84c322ff0c38da88e0da4c65ba892e23a95
Amar <007bhanwaria@gmail.com>
2025-04-18 02:48:12 +0800

    Auth setup done

--- Spotify/Spotify/Manager/AuthManager.swift
+++ Spotify/Spotify/Manager/AuthManager.swift
@@ -11,6 +11,21 @@ final class AuthManager {
     
     static let shared = AuthManager()
     
+    struct Constants {
+        static let clientID = "46c1bdd1fe8e4433bc4c091277e0cccb"
+        static let clientSecret = "a7bb10052f79472c97acdaa22d8a8d2d"
+        static let redirectURI = "https://www.google.com"
+
+    }
+    
+    public var signInURL: URL? {
+        let scopes = "user-read-private"
+        let base = "https://accounts.spotify.com/authorize"
+        let redirectURIUnCoded = Constants.redirectURI.addingPercentEncoding(withAllowedCharacters: .urlUserAllowed)
+        let string = "\(base)?response_type=code&client_id=\(Constants.clientID)&scope=\(scopes)&redirect_uri=\(redirectURIUnCoded!)&show_dialog=TRUE"
+        return URL(string: string)
+    }
+    
     private init() {}
     
     var isSignedIn: Bool { return false }
@@ -23,4 +38,8 @@ final class AuthManager {
     
     private var shouldRefreshToken: Bool { return false }
     
+    public func exchangeCodeForToken(code: String, completion: @escaping (Bool) -> Void) {
+        
+        
+    }
 }
--- Spotify/Spotify/ViewControllers/Others/AuthViewController.swift
+++ Spotify/Spotify/ViewControllers/Others/AuthViewController.swift
@@ -6,12 +6,78 @@
 //
 
 import UIKit
+import WebKit
 
 class AuthViewController: UIViewController {
 
+    // MARK: - UI Components
+    private let webView: WKWebView = {
+        let preferences = WKWebpagePreferences()
+        preferences.allowsContentJavaScript = true // Enable JavaScript for OAuth flow
+        let configuration = WKWebViewConfiguration()
+        configuration.defaultWebpagePreferences = preferences
+        return WKWebView(frame: .zero, configuration: configuration)
+    }()
+    
+    // MARK: - Properties
+    public var completionHandler: ((Bool) -> Void)?
+    
+    // MARK: - Lifecycle Methods
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupView()
+        loadSignInPage()
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        webView.frame = view.bounds
+    }
+}
 
-        self.view.backgroundColor = .systemBackground
+// MARK: - Setup Methods
+extension AuthViewController {
+    private func setupView() {
+        view.backgroundColor = .systemBackground
+        title = "Sign In"
+        webView.navigationDelegate = self
+        view.addSubview(webView)
+    }
+    
+    private func loadSignInPage() {
+        guard let url = AuthManager.shared.signInURL else {
+            return
+        }
+        
+        webView.load(URLRequest(url: url))
+    }
+}
+
+// MARK: - WKNavigationDelegate
+extension AuthViewController: WKNavigationDelegate {
+    
+    func webView(_ webView: WKWebView, didStartProvisionalNavigation navigation: WKNavigation!) {
+        guard let url = webView.url else { return }
+        handlePotentialRedirect(url: url)
+    }
+    
+    private func handlePotentialRedirect(url: URL) {
+        // Ensure this is our redirect URL
+        guard url.absoluteString.hasPrefix(AuthManager.Constants.redirectURI) else { return }
+        
+        // Extract authorization code from URL query parameters
+        let components = URLComponents(string: url.absoluteString)
+        guard let code = components?.queryItems?.first(where: { $0.name == "code" })?.value else {
+            completionHandler?(false)
+            return
+        }
+        
+        // Exchange authorization code for access token
+        AuthManager.shared.exchangeCodeForToken(code: code) { [weak self] success in
+            DispatchQueue.main.async {
+                self?.completionHandler?(success)
+                self?.navigationController?.popViewController(animated: true)
+            }
+        }
     }
 }
